d898816952008183c99848964e5a0cf29e16947f
leonardo.giorcelli <leonardo.giorcelli@pedidosya.com>
2020-06-07 20:05:51 -0300

    Implementation and test for Faction service

--- src/main/kotlin/katas/rpg/model/faction/DefaultFactionService.kt
+++ src/main/kotlin/katas/rpg/model/faction/DefaultFactionService.kt
@@ -4,12 +4,33 @@ import katas.rpg.model.LeveledCharacter
 
 interface FactionService {
     fun belongsToSameFaction(attacker: LeveledCharacter, attacked: LeveledCharacter): Boolean
+    fun addMemberToFaction(character: LeveledCharacter, factionName: String)
+    fun belongsToFaction(character: LeveledCharacter, factionName: String): Boolean
 }
 
 class DefaultFactionService : FactionService {
+    private val factions: MutableMap<String, List<LeveledCharacter>> = mutableMapOf()
 
     override fun belongsToSameFaction(attacker: LeveledCharacter, attacked: LeveledCharacter): Boolean {
-        TODO("Not yet implemented")
+        val firstSetOfFactions = factionsOf(attacker)
+        val secondSetOfFactions = factionsOf(attacked)
+        return firstSetOfFactions.intersect(secondSetOfFactions).isNotEmpty()
+    }
+
+    private fun factionsOf(attacker: LeveledCharacter): Set<String> {
+        return factions.filter { (_, members) -> members.contains(attacker) }.keys
+    }
+
+    override fun addMemberToFaction(character: LeveledCharacter, factionName: String) {
+        var members = factions[factionName]
+        if (members == null) {
+            members = emptyList()
+        }
+        factions[factionName] = members + character
+    }
+
+    override fun belongsToFaction(character: LeveledCharacter, factionName: String): Boolean {
+        return factions[factionName]?.contains(character) ?: false
     }
 
 }
---
+++ src/test/kotlin/katas/rpg/model/faction/DefaultFactionServiceTest.kt
@@ -0,0 +1,54 @@
+package katas.rpg.model.faction
+
+import com.nhaarman.mockito_kotlin.mock
+import katas.rpg.model.LeveledCharacter
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+
+class DefaultFactionServiceTest {
+    lateinit var service: DefaultFactionService
+
+    @BeforeEach
+    internal fun setUp() {
+        service = DefaultFactionService()
+    }
+
+    @Test
+    fun `should add members to factions`() {
+        val character: LeveledCharacter  = mock()
+        val factionName = "firstFaction"
+
+        service.addMemberToFaction(character, factionName)
+
+        Assertions.assertTrue(service.belongsToFaction(character, factionName))
+        Assertions.assertFalse(service.belongsToFaction(character, "otherFaction"))
+    }
+
+    @Test
+    fun `should be able to inform if two character are part of the same faction`() {
+        val factionName = "sharedFaction"
+        val character: LeveledCharacter  = mock()
+        val otherCharacter: LeveledCharacter  = mock()
+        service.addMemberToFaction(character, factionName)
+        service.addMemberToFaction(otherCharacter, factionName)
+
+        val belongsToSameFaction = service.belongsToSameFaction(character, otherCharacter)
+
+        Assertions.assertTrue(belongsToSameFaction)
+    }
+
+    @Test
+    fun `should return false  if two character are not part of the same faction`() {
+        val factionName = "firstFaction"
+        val otherFactionName = "otherFaction"
+        val character: LeveledCharacter  = mock()
+        val otherCharacter: LeveledCharacter  = mock()
+        service.addMemberToFaction(character, factionName)
+        service.addMemberToFaction(otherCharacter, otherFactionName)
+
+        val belongsToSameFaction = service.belongsToSameFaction(character, otherCharacter)
+
+        Assertions.assertFalse(belongsToSameFaction)
+    }
+}
